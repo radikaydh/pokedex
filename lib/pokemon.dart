15767ccacb82cec1ef75623f93a56aecb12440b3
Radika Yudha <70556101+radikaydh@users.noreply.github.com>
2021-02-23 12:17:42 +0700

    new update

--- lib/pokemon.dart
+++ lib/pokemon.dart
@@ -31,6 +31,8 @@ class BasePokemon {
   }
 
   Map<String, dynamic> toJson() => {"name": name, "url": url};
+
+  get(String s) {}
 }
 
 Pokemon pokemonFromJson(String str) => Pokemon.fromJson(json.decode(str));
@@ -82,6 +84,8 @@ class Pokemon {
           json["abilities"].map((x) => Ability.fromJson(x)),
         ),
       );
+
+  get(String s) {}
 }
 
 class Ability {
@@ -129,46 +133,18 @@ class Species {
 }
 
 class Sprites {
-  String backDefault;
-  dynamic backFemale;
-  String backShiny;
-  dynamic backShinyFemale;
   String frontDefault;
-  dynamic frontFemale;
-  String frontShiny;
-  dynamic frontShinyFemale;
 
   Sprites({
-    this.backDefault,
-    this.backFemale,
-    this.backShiny,
-    this.backShinyFemale,
     this.frontDefault,
-    this.frontFemale,
-    this.frontShiny,
-    this.frontShinyFemale,
   });
 
   factory Sprites.fromJson(Map<String, dynamic> json) => Sprites(
-        backDefault: json["back_default"],
-        backFemale: json["back_female"],
-        backShiny: json["back_shiny"],
-        backShinyFemale: json["back_shiny_female"],
         frontDefault: json["front_default"],
-        frontFemale: json["front_female"],
-        frontShiny: json["front_shiny"],
-        frontShinyFemale: json["front_shiny_female"],
       );
 
   Map<String, dynamic> toJson() => {
-        "back_default": backDefault,
-        "back_female": backFemale,
-        "back_shiny": backShiny,
-        "back_shiny_female": backShinyFemale,
         "front_default": frontDefault,
-        "front_female": frontFemale,
-        "front_shiny": frontShiny,
-        "front_shiny_female": frontShinyFemale,
       };
 }
 
